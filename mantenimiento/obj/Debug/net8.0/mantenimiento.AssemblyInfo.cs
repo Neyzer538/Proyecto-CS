1d0356937f213345f67ac52d12b31bb9f6e73ac8
Neyzer538 <124699314+Neyzer538@users.noreply.github.com>
2025-05-01 19:19:35 -0600

    Se agrega el archivo .gitignore

--- mantenimiento/obj/Debug/net8.0/mantenimiento.AssemblyInfo.cs
+++ mantenimiento/obj/Debug/net8.0/mantenimiento.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mantenimiento")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+084d4527f15fc043bca5d522adccbc1c0aabce4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("mantenimiento")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mantenimiento")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
